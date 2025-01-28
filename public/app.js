cfa75e02e9b7d4028d15667fb00e0aa0f2c210af
Vikyath  Naradasi <vikyath@Vikyaths-MacBook-Pro.local>
2025-01-28 02:39:57 -0800

    Security Changes

--- public/app.js
+++ public/app.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyACUIzdxt-MxlzZJWG-MCcuRIwGXiGx1EU",
-  authDomain: "personal-website-ef637.firebaseapp.com",
-  projectId: "personal-website-ef637",
-  storageBucket: "personal-website-ef637.firebasestorage.app",
-  messagingSenderId: "269861284343",
-  appId: "1:269861284343:web:1598ca301cf9620fa6a6f2",
-  measurementId: "G-W38KTHC2GE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
